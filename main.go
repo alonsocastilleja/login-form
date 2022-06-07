80a4c317e1d997a24e2d3f4ce073c1d3a0546d57
Alonso Castilleja <castilleja.alonso@gmail.com>
2022-06-07 08:59:46 -0400

    added login and login submit functionality

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
+package main
+
 import (
   "fmt"
   "net/http"
+  "html/template"
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
+  var fileName = "login.html"
+  t, err := template.ParseFiles(fileName)
+  if err != nil {
+    fmt.Println("Error when parsing file", err)
+    return
+  }
 
+  err = t.ExecuteTemplate(w, fileName, data:nil) {
+    if err != nil {
+      fmt.Println("Error when executing template", err)
+      return
+    }
+  }
+}
+
+// Creating a mock DB
+var mockDB = map[string]string {
+  "Wallace": "goodPassword"
 }
 
 func loginSubmit(w http.ResponseWriter, r *http.Request) {
-
+  username := r.FormValue("username")
+  password := r.FormValue("password")
+  fmt.Println(username, password)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,4 +40,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
     case "/login-submit":
       loginSubmit(w, r)
     }
-}
+    default: 
+      fmt.Fprintf(w, format: "Sup Ninjas")
+} 
